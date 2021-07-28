cff6d19007ed12a4258ad2db2393cbbba9cd46b8
おう <15621624+xbw@users.noreply.github.com>
2021-07-28 16:25:42 +0800

    spring-boot-base/spring-boot-actuator

---
+++ spring-boot-base/spring-boot-actuator/src/main/java/com/xbw/spring/boot/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.xbw.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
