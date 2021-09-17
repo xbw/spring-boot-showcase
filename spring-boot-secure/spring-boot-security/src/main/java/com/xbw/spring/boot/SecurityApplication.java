2abcef038326382350422b6638f7b24622c537c6
おう <15621624+xbw@users.noreply.github.com>
2021-09-17 08:45:09 +0800

    spring-boot-secure Fix

--- spring-boot-secure/spring-boot-security/src/main/java/com/xbw/spring/boot/SecurityApplication.java
+++ spring-boot-secure/spring-boot-security/src/main/java/com/xbw/spring/boot/SecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class SecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(SecurityApplication.class, args);
     }
 
 }
--- spring-boot-secure/spring-boot-shiro/src/main/java/com/xbw/spring/boot/ShiroApplication.java
+++ spring-boot-secure/spring-boot-shiro/src/main/java/com/xbw/spring/boot/ShiroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class ShiroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(ShiroApplication.class, args);
     }
 
 }
